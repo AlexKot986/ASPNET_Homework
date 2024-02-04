a2e69917b4b40efc454f94d6b350ef5cc310ad3d
Alexershov86 <alexershov86@mail.ru>
2024-02-04 15:23:32 +0300

    aspnet homework1

---
+++ StoreMarket/Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreMarket.Contexts;
+using StoreMarket.Contracts.Requests;
+using StoreMarket.Contracts.Responses;
+using StoreMarket.Models;
+
+namespace StoreMarket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private StoreContext storeContext;
+
+        public CategoryController(StoreContext storeContext)
+        {
+            this.storeContext = storeContext;
+        }
+
+        [HttpPost]
+        [Route("category")]
+        public ActionResult<CategoryResponse> AddCategory(CategoryCreateRequest request)
+        {
+            Category category = request.CategoryGetEntity();
+            try
+            {
+                var result = storeContext.Categories.Add(category).Entity;
+                storeContext.SaveChanges();
+                return Ok(new CategoryResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("category/{id}")]
+
+        public ActionResult<CategoryResponse> GetCategory(int id)
+        {
+            var result = storeContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(new CategoryResponse(result));
+            }
+
+        }
+
+        [HttpGet]
+        [Route("categories")]
+
+        public ActionResult<IEnumerable<CategoryResponse>> GetCategories()
+        {
+            var result = storeContext.Categories;
+
+            return Ok(result.Select(result => new CategoryResponse(result)));
+        }
+
+        [HttpDelete]
+        [Route("category/{id}")]
+        public ActionResult<CategoryResponse> DeleteCategory(int id)
+        {
+            var result = storeContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                storeContext.Categories.Remove(result);
+                storeContext.SaveChanges();
+                return Ok();
+            }
+
+        }
+
+        [HttpPut]
+        [Route("category/{id}")]
+        public ActionResult<CategoryResponse> UpdateCategory(int id, CategoryUpdateRequest request)
+        {
+            Category category = request.CategoryGetEntity(id);
+            try
+            {
+                var result = storeContext.Categories.Update(category).Entity;
+                storeContext.SaveChanges();
+                return Ok(new CategoryResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ StoreMarket/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreMarket.Contexts;
+using StoreMarket.Contracts.Requests;
+using StoreMarket.Contracts.Responses;
+using StoreMarket.Models;
+
+namespace StoreMarket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private StoreContext storeContext;
+        public ProductController(StoreContext context)
+        {
+            storeContext = context;
+        }
+
+        [HttpGet]
+        [Route("product/{id}")]
+
+        public ActionResult<ProductResponse> GetProduct(int id)
+        {
+            var result = storeContext.Products.FirstOrDefault(p => p.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(new ProductResponse(result));
+            }
+
+        }
+
+
+        [HttpGet]
+        [Route("products")]
+
+        public ActionResult<IEnumerable<ProductResponse>> GetProducts()
+        {
+            var result = storeContext.Products;
+
+            return Ok(result.Select(result => new ProductResponse(result)));
+        }
+
+        [HttpPost]
+        [Route("product")]
+
+        public ActionResult<ProductResponse> AddProduct(ProductCreateRequest request)
+        {
+            Product product = request.ProductGetEntity();
+            try
+            {
+                var result = storeContext.Products.Add(product).Entity;
+
+                storeContext.SaveChanges();
+                return Ok(new ProductResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("product/{id}")]
+
+        public ActionResult<ProductResponse> DeleteProduct(int id)
+        {
+            var result = storeContext.Products.FirstOrDefault(p => p.Id == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                storeContext.Products.Remove(result);
+                storeContext.SaveChanges();
+                return Ok();
+            }
+
+        }
+
+        [HttpPut]
+        [Route("product/{id}")]
+        public ActionResult<ProductResponse> UpdateProduct(int id, ProductUpdateRequest request)
+        {
+
+            Product product = request.ProductGetEntity(id);
+            try
+            {
+                var result = storeContext.Products.Update(product).Entity;
+                storeContext.SaveChanges();
+                return Ok(new ProductResponse(result));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
